6309db1ff3eb3b4790c17d3ad10e1f9066b26d32
Yaroslava Myslyva <yaroslava.met@gmail.com>
2023-12-28 12:29:10 +0200

    Can only greet and not recognize the command

--- src/main/java/com/example/example_bot/bot/ExampleBot.java
+++ src/main/java/com/example/example_bot/bot/ExampleBot.java
@@ -1,23 +1,80 @@
 package com.example.example_bot.bot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Component
 public class ExampleBot extends TelegramLongPollingBot {
 
-   public ExampleBot(String botToken) {
+   public ExampleBot(@Value("${bot.token}") String botToken) {
        super(botToken);
    }
 
-    @Override
-    public void onUpdateReceived(Update update) {
 
-    }
 
     @Override
     public String getBotUsername() {
-        return null;
+
+       return "Мой бот 2";
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleBot.class);
+
+    private static final String START = "/start";
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
+            return;
+        }
+        var message = update.getMessage().getText();
+        var chatId = update.getMessage().getChatId();
+        switch (message) {
+            case START -> {
+                String userName = update.getMessage().getChat().getUserName();
+                startCommand(chatId, userName);
+            }
+
+            default -> unknownCommand(chatId);
+        }
+    }
+
+
+
+    private void startCommand(Long chatId, String userName) {
+        var text = """
+                Добро пожаловать в бот, %s!
+                
+                """;
+        var formattedText = String.format(text, userName);
+        sendMessage(chatId, formattedText);
+    }
+
+
+
+
+
+    private void unknownCommand(Long chatId) {
+        var text = "Не удалось распознать команду!";
+        sendMessage(chatId, text);
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        var chatIdStr = String.valueOf(chatId);
+        var sendMessage = new SendMessage(chatIdStr, text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            LOG.error("Ошибка отправки сообщения", e);
+        }
     }
 }
